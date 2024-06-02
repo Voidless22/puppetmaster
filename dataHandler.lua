bc8141723ca66ae8e07e7733fdeab551bc0bbb78
Voidless22 <falant221@gmail.com>
2024-06-02 13:43:23 -0400

    rewrote data handler to be much more performant

--- dataHandler.lua
+++ dataHandler.lua
@@ -1,14 +1,26 @@
 local mq = require('mq')
-local actors = require('actors')
-local timer = require('timer')
+
 local dataHandler = {}
 
 dataHandler.boxes = {}
+dataHandler.updateQueue = {}
 
+function dataHandler.AddNewCharacter(name)
+    if not dataHandler.boxes[name] then
+        dataHandler.boxes[name] = {}
+
+        printf('\agData Manager: \awEntry created in Character Data table: \at%s', name)
+    else
+        printf('\agData Manager: \arCharacter Data Entry creation attempted, but already exists.')
+    end
+end
+
+function dataHandler.GetData(boxName)
+    if dataHandler.boxes[boxName] then
+        return dataHandler.boxes[boxName]
+    end
+end
 local spellTable = {}
-local spellCategories = {}
-local spellSubCategories = {}
-
 local function buildSpellTable()
     for i = 1, 720 do
         if mq.TLO.Me.Book(i).ID() then
@@ -31,204 +43,143 @@ local function buildSpellTable()
 
     return spellTable
 end
-
-function dataHandler.AddNewCharacter(charName)
-    if not dataHandler.boxes[charName] then
-        dataHandler.boxes[charName] = {}
-
-        printf('\agDataHandler: \awEntry created in Character Data table: \at%s', charName)
-    else
-        printf('\agDataHandler: \arCharacter Data Entry creation attempted, but already exists.')
-    end
-end
-
-function dataHandler.GetData(charName)
-    if dataHandler.boxes[charName] then
-        return dataHandler.boxes[charName]
-    end
-end
-
-local function updateBuffs(boxName)
-    local currentBoxIndex = dataHandler.boxes[boxName]
-    for index = 1, mq.TLO.Me.MaxBuffSlots() do
-        local buffFound = false
-        if mq.TLO.Me.Buff(index)() ~= nil then
-            for _, value in ipairs(currentBoxIndex.Buffs) do
-                if value == mq.TLO.Me.Buff(index).Spell.ID() then
-                    buffFound = true
-                end
-            end
-            if not buffFound then
-                currentBoxIndex.Buffs[index] = {
-                    id = mq.TLO.Me.Buff(index).Spell.ID(),
-                    duration = mq.TLO.Me.Buff(index)
-                        .Duration.TimeHMS()
-                }
-            end
-        else
-            currentBoxIndex.Buffs[index] = { id = 0, duration = 0 }
-        end
-    end
-end
-
-local function updateTargetBuffs(boxName)
-    local currentBoxIndex = dataHandler.boxes[boxName]
-    local targetBuffCount = mq.TLO.Target.BuffCount()
-    if targetBuffCount == 0 then
-        for i = 0, #currentBoxIndex.Target.Buffs do
-            currentBoxIndex.Target.Buffs[i] = nil
-        end
-    elseif mq.TLO.Target.BuffCount() and mq.TLO.Target.ID() ~= 0 then
-        for i = 0, targetBuffCount do
-            local buffFound = false
-            if mq.TLO.Target.Buff(i).SpellID() ~= nil and currentBoxIndex.Target.Buffs[i] ~= mq.TLO.Target.Buff(i).SpellID() then
-                for _, value in pairs(currentBoxIndex.Target.Buffs) do
-                    if value == mq.TLO.Target.Buff(i).SpellID() then
-                        buffFound = true
-                    end
-                end
-            end
-            if not buffFound then
-                currentBoxIndex.Target.Buffs[i] = mq.TLO.Target.Buff(i).SpellID()
-            end
-        end
-    end
-end
-
-local function updateSpellbar(boxName)
-    local currentBoxIndex = dataHandler.boxes[boxName]
-    for gem = 1, mq.TLO.Me.NumGems() do
-        if mq.TLO.Me.Gem(gem).ID() == nil then
-            currentBoxIndex.Spellbar[gem] = 0
-        else
-            currentBoxIndex.Spellbar[gem] = mq.TLO.Me.Gem(gem).ID()
-            if not mq.TLO.Me.SpellReady(gem)() then
-                currentBoxIndex.SpellCooldowns[gem] = mq.TLO.Me.Gem(gem).RecastTime()
-            else
-                currentBoxIndex.SpellCooldowns[gem] = 0
-            end
-        end
-    end
-end
-local function updateGroup(boxName)
-    local foundSelf = false
-    local currentBoxIndex = dataHandler.boxes[boxName]
-    for member = 1, mq.TLO.Me.GroupSize() do
-        currentBoxIndex.Group[member] = mq.TLO.Group.Member(member).Name()
-    end
-    for index, value in ipairs(currentBoxIndex.Group) do
-        if value == mq.TLO.Me.Name() then
-            foundSelf = true
-        end
-    end
-    if not foundSelf then
-        table.insert(currentBoxIndex.Group, mq.TLO.Me.Name())
-    end
-end
-
-local function updateXTarget(boxName)
-    local currentBoxIndex = dataHandler.boxes[boxName]
-    for i = 1, mq.TLO.Me.XTargetSlots() do
-        if mq.TLO.Me.XTarget(i).ID() == nil or mq.TLO.Me.XTarget(i).ID() == 0 then
-            if not currentBoxIndex.XTarget[i] then
-                currentBoxIndex.XTarget[i] = { Id = 0, ConColor = 0, AggroPct = 0 }
-            end
-            currentBoxIndex.XTarget[i].Id = 0
-            currentBoxIndex.XTarget[i].ConColor = 0
-            currentBoxIndex.XTarget[i].AggroPct = 0
-        end
-        if mq.TLO.Me.XTarget(i).ID() ~= nil and mq.TLO.Me.XTarget(i).ID() ~= 0 then
-            if not currentBoxIndex.XTarget[i] then
-                currentBoxIndex.XTarget[i] = { Id = 0, ConColor = 0, AggroPct = 0 }
-            end
-
-            currentBoxIndex.XTarget[i].Id = mq.TLO.Me.XTarget(i).ID()
-            currentBoxIndex.XTarget[i].ConColor = mq.TLO.Me.XTarget(i).ConColor()
-            currentBoxIndex.XTarget[i].AggroPct = mq.TLO.Me.XTarget(i).PctAggro()
-        end
-    end
-end
-
-local function updatePet(boxName)
-    local currentBoxIndex = dataHandler.boxes[boxName]
-    -- No Pet
-    if mq.TLO.Me.Pet() == "NO PET" then
-        currentBoxIndex.Pet = "NO PET"
-    end
-    -- Pet Summoned
-    if mq.TLO.Me.Pet() ~= 'NO PET' then
-        currentBoxIndex.Pet = mq.TLO.Spawn(mq.TLO.Me.Pet()).ID()
-        -- in combat
-        if mq.TLO.Me.Pet.Combat() ~= currentBoxIndex.PetInCombat then
-            currentBoxIndex.PetInCombat = mq.TLO.Me.Pet.Combat()
-        end
-        -- Target
-        if mq.TLO.Me.Pet.Target.ID() == 0 or mq.TLO.Me.Pet.Target.Dead() then
-            currentBoxIndex.PetTarget = 'Empty'
-        else
-            currentBoxIndex.PetTarget = mq.TLO.Me.Pet.Target.ID()
-        end
-        -- Taunt
-        currentBoxIndex.PetTaunt = mq.TLO.Me.Pet.Taunt()
-    end
-end
-
-
 function dataHandler.InitializeData(boxName)
-    local currentBoxIndex = dataHandler.boxes[boxName]
-    currentBoxIndex.PctHP = mq.TLO.Me.PctHPs() or 0
-    currentBoxIndex.PctMana = mq.TLO.Me.PctMana() or 0
-    currentBoxIndex.PctEnd = mq.TLO.Me.PctEndurance()
-    currentBoxIndex.Level = mq.TLO.Me.Level()
-    currentBoxIndex.Class = mq.TLO.Me.Class()
-    currentBoxIndex.Race = mq.TLO.Me.Race()
-    if not currentBoxIndex.Buffs then currentBoxIndex.Buffs = {} end
-    if not currentBoxIndex.XTarget then currentBoxIndex.XTarget = {} end
-    if not currentBoxIndex.Spellbar then currentBoxIndex.Spellbar = {} end
-    if not currentBoxIndex.Group then currentBoxIndex.Group = {} end
-    currentBoxIndex.Target = { id = mq.TLO.Target.ID(), ConColor = mq.TLO.Target.ConColor(), AggroPct = mq.TLO.Target.PctAggro() }
-    if not currentBoxIndex.Target.Buffs then
-        currentBoxIndex.Target.Buffs = {}
-    end
+    dataHandler.boxes[boxName] = {}
+    local cBox = dataHandler.boxes[boxName]
+    cBox.PctHP = mq.TLO.Me.PctHPs()
+    cBox.PctMana = mq.TLO.Me.PctMana()
+    cBox.BuffIds = {}
+    cBox.BuffDurations = {}
+    cBox.Spellbar = {}
+    cBox.SpellCooldowns = {}
+    cBox.Group = {}
+    cBox.Target = { id = mq.TLO.Target.ID(), ConColor = mq.TLO.Target.ConColor(), AggroPct = mq.TLO.Target.PctAggro() }
+    cBox.TargetBuffs = {}
+    cBox.Spellbook = buildSpellTable()
+    cBox.CombatAbilities = {}
+    cBox.PetFollow = true
+    cBox.PetTaunt = mq.TLO.Me.Pet.Taunt()
+    cBox.followMATarget = false
+    cBox.chaseToggle = false
+    cBox.meleeTarget = false
+    cBox.Sitting = mq.TLO.Me.Sitting()
+    cBox.isCasting = false
+    cBox.lastCastGem = 0
+    cBox.CombatState = mq.TLO.Me.CombatState()
+    cBox.CastTimeLeft = mq.TLO.Me.CastTimeLeft.Seconds()
+    cBox.xtIDs = {}
+    cBox.xtConColors = {}
+    cBox.xtAggroPcts = {}
+end
 
-    currentBoxIndex.Spellbook = buildSpellTable()
-    currentBoxIndex.CombatAbilities = {}
-    currentBoxIndex.PetFollow = true
-    currentBoxIndex.PetTaunt = mq.TLO.Me.Pet.Taunt()
-    currentBoxIndex.followMATarget = false
-    currentBoxIndex.chaseToggle = false
-    currentBoxIndex.meleeTarget = false
-    currentBoxIndex.Sitting = mq.TLO.Me.Sitting()
-    currentBoxIndex.isCasting = mq.TLO.Me.Casting()
-    currentBoxIndex.lastCastGem = 0
-    currentBoxIndex.CombatState = mq.TLO.Me.CombatState()
-    currentBoxIndex.CastTimeLeft = mq.TLO.Me.CastTimeLeft.Seconds()
-    currentBoxIndex.SpellCooldowns = {}
-    currentBoxIndex.XTargetConColors = {}
-    currentBoxIndex.XTargetAggroPcts = {}
+function dataHandler.UpdateCheck(dataIndex, prevData, currentData, subIndex)
+    if dataHandler.updateQueue[dataIndex] == nil then
+        dataHandler.updateQueue[dataIndex] = {}
+    end
+    -- data the same? Clear the Queue entry
+    if prevData == currentData and subIndex == nil then
+        dataHandler.updateQueue[dataIndex] = nil
+    elseif prevData == currentData and subIndex ~= nil then
+        dataHandler.updateQueue[dataIndex][subIndex] = nil
+    elseif subIndex == nil then
+        dataHandler.updateQueue[dataIndex] = currentData
+        return
+    elseif subIndex ~= nil then
+        dataHandler.updateQueue[dataIndex][subIndex] = currentData
+        return
+    end
+end
+local function returnIsCasting()
+    if mq.TLO.Me.Casting() then
+        return true
+    else
+        return false
+    end
+end
+
+local function returnSpellCD(gem)
+    if not mq.TLO.Me.SpellReady(gem)() then
+        return mq.TLO.Me.Gem(gem).RecastTime()
+    else
+        return 0
+    end
+end
+
+function dataHandler.UpdateSpellbar(boxName)
+    local cBox = dataHandler.boxes[boxName]
+    for gem = 1, mq.TLO.Me.NumGems() do
+        dataHandler.UpdateCheck("Spellbar", cBox.Spellbar[gem], (mq.TLO.Me.Gem(gem).ID() or 0), gem)
+        dataHandler.UpdateCheck("SpellCooldowns", cBox.SpellCooldowns[gem], returnSpellCD(gem), gem)
+    end
+end
+
+function dataHandler.UpdateBuffs(boxName)
+    local cBox = dataHandler.boxes[boxName]
+    for i = 1, mq.TLO.Me.MaxBuffSlots() do
+        dataHandler.UpdateCheck("BuffIds", cBox.BuffIds[i], (mq.TLO.Me.Buff(i).Spell.ID() or 0), i)
+        dataHandler.UpdateCheck("BuffDurations", cBox.BuffDurations[i], (mq.TLO.Me.Buff(i).Duration.TimeHMS() or 0), i)
+    end
+end
+
+function dataHandler.UpdateGroup(boxName)
+    local cBox = dataHandler.boxes[boxName]
+    for i = 0, mq.TLO.Me.GroupSize() do
+        dataHandler.UpdateCheck("Group", cBox.Group[i], mq.TLO.Group.Member(i).Name(), i)
+    end
+end
+
+function dataHandler.UpdateXTarget(boxName)
+    local cBox = dataHandler.boxes[boxName]
+    for i = 1, mq.TLO.Me.XTargetSlots() do
+        dataHandler.UpdateCheck("xtIDs", cBox.xtIDs[i], (mq.TLO.Me.XTarget(i).ID() or 0), i)
+        dataHandler.UpdateCheck("xtConColors", cBox.xtConColors[i], (mq.TLO.Me.XTarget(i).ConColor() or 0), i)
+        dataHandler.UpdateCheck("xtAggroPcts", cBox.xtAggroPcts[i], (mq.TLO.Me.XTarget(i).PctAggro() or 0), i)
+    end
+end
+
+function dataHandler.UpdateTargetBuffs(boxName)
+    local cBox = dataHandler.boxes[boxName]
+
+    for i = 1, (mq.TLO.Target.BuffCount() or 1) do
+        dataHandler.UpdateCheck("TargetBuffs", cBox.TargetBuffs[i], mq.TLO.Target.Buff(i).SpellID(), i)
+    end
+end
+
+function dataHandler.ProcessQueue(boxName)
+    local cBox = dataHandler.boxes[boxName]
+    for index, value in pairs(dataHandler.updateQueue) do
+        if type(value) == "table" then
+            for subtable, data in pairs(value) do
+                if cBox[index] then
+                    cBox[index][subtable] = data
+                    printf("Updating %s: Subtable: %s value:%s", index, subtable, data)
+                end
+            end
+        else
+            printf('Updating: %s to %s', index, value)
+            cBox[index] = value
+        end
+    end
 end
 
 function dataHandler.UpdateData(boxName)
-    local currentBoxIndex = dataHandler.boxes[boxName]
-    if currentBoxIndex then
-        currentBoxIndex.CastTimeLeft = mq.TLO.Me.CastTimeLeft.Seconds()
-
-        currentBoxIndex.Sitting = mq.TLO.Me.Sitting()
-        currentBoxIndex.isCasting = mq.TLO.Me.Casting()
-        currentBoxIndex.PctHP = mq.TLO.Me.PctHPs()
-        currentBoxIndex.PctMana = mq.TLO.Me.PctMana()
-        currentBoxIndex.PctEnd = mq.TLO.Me.PctEndurance()
-        currentBoxIndex.CombatState = mq.TLO.Me.CombatState()
-        updateBuffs(boxName)
-        currentBoxIndex.Target.id = mq.TLO.Target.ID()
-        currentBoxIndex.Target.ConColor = mq.TLO.Target.ConColor()
-        currentBoxIndex.Target.AggroPct = mq.TLO.Target.PctAggro()
-        updateTargetBuffs(boxName)
-        updateSpellbar(boxName)
-        updateGroup(boxName)
-        updateXTarget(boxName)
-        updatePet(boxName)
-    end
+    local cBox = dataHandler.boxes[boxName]
+    dataHandler.UpdateCheck("Sitting", cBox.Sitting, mq.TLO.Me.Sitting())
+    dataHandler.UpdateCheck("isCasting", cBox.isCasting, returnIsCasting())
+    dataHandler.UpdateCheck("PctHP", cBox.PctHP, mq.TLO.Me.PctHPs())
+    dataHandler.UpdateCheck("PctMana", cBox.PctMana, mq.TLO.Me.PctMana())
+    dataHandler.UpdateCheck("PctEnd", cBox.PctEnd, mq.TLO.Me.PctEndurance())
+    dataHandler.UpdateCheck("CombatState", cBox.CombatState, mq.TLO.Me.CombatState())
+    dataHandler.UpdateCheck("Target", cBox.Target.id, (mq.TLO.Target.ID() or 0), "id")
+    dataHandler.UpdateCheck("Target", cBox.Target.ConColor, (mq.TLO.Target.ConColor() or 0), "ConColor")
+    dataHandler.UpdateCheck("Target", cBox.Target.AggroPct, (mq.TLO.Target.PctAggro() or 0), "AggroPct")
+    dataHandler.UpdateCheck("CastTimeLeft", cBox.CastTimeLeft, mq.TLO.Me.CastTimeLeft.Seconds())
+    dataHandler.UpdateSpellbar(boxName)
+    dataHandler.UpdateGroup(boxName)
+    dataHandler.UpdateXTarget(boxName)
+    dataHandler.UpdateBuffs(boxName)
+    dataHandler.UpdateTargetBuffs(boxName)
+    dataHandler.ProcessQueue(boxName)
 end
 
 return dataHandler
--- windows/buffWindow.lua
+++ windows/buffWindow.lua
@@ -10,7 +10,7 @@ local animSpellIcons = mq.FindTextureAnimation('A_SpellIcons')
 function buffWindow.DrawBuffWindow(charName, charTable)
     ImGui.SetWindowSize("Buffs-" .. charName, 150, 150, ImGuiCond.FirstUseEver)
     ImGui.Text('Buffs: %s', charName)
-    local buffNames = charTable.Buffs
+    local buffNames = charTable.BuffIds
     local windowWidth = ImGui.GetWindowSizeVec().x
     local columnCount = math.floor(windowWidth / 32)
     local currentColumn = 1
@@ -18,8 +18,8 @@ function buffWindow.DrawBuffWindow(charName, charTable)
 
     if buffNames ~= nil then
         for index, data in ipairs(buffNames) do
-            if data.id ~= 0 then
-                animSpellIcons:SetTextureCell(mq.TLO.Spell(data.id).SpellIcon())
+            if data ~= 0 then
+                animSpellIcons:SetTextureCell(mq.TLO.Spell(data).SpellIcon())
                 if currentColumn < columnCount then
                     local prevX = ImGui.GetCursorPosX()
                     local prevY = ImGui.GetCursorPosY()
@@ -38,15 +38,15 @@ function buffWindow.DrawBuffWindow(charName, charTable)
                 if ImGui.IsItemHovered(ImGuiHoveredFlags.DelayNormal) then
                     if ImGui.IsMouseClicked(ImGuiMouseButton.Left) then
                     if charName ~= mq.TLO.Me.Name() then
-                        utils.driverActor:send(msgHandler.boxAddress, {id='removeBuff', buff=data.id, charName = charName})
+                        utils.driverActor:send(msgHandler.boxAddress, {id='removeBuff', buff=data, charName = charName})
                     else
-                        mq.TLO.Me.Buff(mq.TLO.Spell(data.id).Name()).Remove()
+                        mq.TLO.Me.Buff(mq.TLO.Spell(data).Name()).Remove()
                     end
 
                     end
                     if ImGui.BeginTooltip() then
-                        if data.id ~= 0 then
-                        ImGui.Text(mq.TLO.Spell(data.id).Name()..' '.. data.duration)
+                        if data ~= 0 then
+                        ImGui.Text(mq.TLO.Spell(data).Name()..' '.. charTable.BuffDurations[index])
                         else
                         ImGui.Text('Empty')
                         end
